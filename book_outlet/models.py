3bb14bc9882628cba423feccb3cda52f58b8bf81
Martin Andrei <andrei.martin28@outlook.com>
2023-09-28 17:14:06 +0300

    08 Relationships- 006 Managing Relations in Admin

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -2,18 +2,29 @@ from django.core import validators
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
-
+from django.utils.text import slugify
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
+
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
-        validators=[MaxValueValidator(1), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=100)
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books") # NULL
     is_bestselling = models.BooleanField(default=False)
+    # Harry Potter 1 => harry-potter-1
+    slug = models.SlugField(default="", blank=True, 
+                             null=False, db_index=True) 
 
     def get_absolute_url(self):
-        return reverse("book-detail", args=[self.id])      
+        return reverse("book-detail", args=[self.slug])      
+
 
     def __str__(self):
         return f"{self.title} ({self.rating})"  
